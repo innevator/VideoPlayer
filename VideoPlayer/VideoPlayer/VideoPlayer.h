aa91971b4598f058605971eaa89627120f7ffeda
innevator <innevatorg@gmail.com>
2024-05-24 06:26:15 +0800

    creat project

---
+++ VideoPlayer/VideoPlayer/VideoPlayer.h
@@ -0,0 +1,18 @@
+//
+//  VideoPlayer.h
+//  VideoPlayer
+//
+//  Created by 洪宗鴻 on 2024/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VideoPlayer.
+FOUNDATION_EXPORT double VideoPlayerVersionNumber;
+
+//! Project version string for VideoPlayer.
+FOUNDATION_EXPORT const unsigned char VideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoPlayer/PublicHeader.h>
+
+
