47f6a7ab8e8b29b66d70c943ae899213a48d1016
innevator <innevatorg@gmail.com>
2024-05-29 16:01:17 +0800

    fix model file does not exist error

---
+++ VideoPlayer/iOSVideoPlayer/Model/Asset.swift
@@ -0,0 +1,22 @@
+//
+//  Asset.swift
+//  VideoPlayer
+//
+//  Created by 洪宗鴻 on 2024/5/16.
+//
+
+import VideoPlayer
+
+class Asset {
+    private let stream: VideoPlayer.Stream
+    
+    var name: String { return stream.name }
+    
+    init(stream: VideoPlayer.Stream) {
+        self.stream = stream
+    }
+    
+    func getStream() -> VideoPlayer.Stream {
+        return stream
+    }
+}
